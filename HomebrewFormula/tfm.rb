5f00b828bf46f23e37b75cb7671d3949ed5bcd68
Carl Javier <carl.javier@hashicorp.com>
2023-03-14 13:55:13 +1100

    test tfm tap

---
+++ HomebrewFormula/tfm.rb
@@ -0,0 +1,32 @@
+class TFM < Formula
+    desc "TFM CLI -- Command Line Interface to migrate TFE workspace to TFC"
+    homepage "https://github.com/hashicorp-services/tfm"
+    url "git@github.com:hashicorp-services/tfm.git", :using => :git, :tag => "0.0.2-pre-alpha"
+    head "git@github.com:hashicorp-services/tfm.git", :using => :git, :branch => "main"
+  
+    depends_on "go" => :build
+  
+    def install
+      if build.head?
+        v_version = Utils.popen_read("git", "describe", "--tags", "--dirty", "--always").chomp
+      else
+        v_version = "v#{version}"
+      end
+      ENV["GOPATH"] = HOMEBREW_CACHE/"go_cache"
+      go_ldflags = "-w -s"
+      go_ldflags << " -X 'github.com/hashicorp-services/tfm/version.Version=#{v_version}'"
+      go_ldflags << " -X 'github.com/hashicorp-services/tfm/version.PackageManager=HomeBrew'"
+      system "go", "build", "-trimpath", "-ldflags", go_ldflags, "-o", bin/"tfm"
+    end
+    def caveats
+      <<~EOS
+
+        See the tfm's documentation for more information:
+        https://go.hashi.co/tfm
+  
+        CHANGELOG:
+        https://github.com/hashicorp-services/tfm/blob/main/CHANGELOG.md
+      EOS
+    end
+  end
+  
---
+++ README.md
@@ -0,0 +1,24 @@
+# homebrew-carljavier
+
+Homebrew Tap Maintained by Carl Javier
+
+Allows for private packages to be easily distributed to people's laptops
+
+## Requirements
+
+[Homebrew](https://brew.sh/)
+
+## Usage / Installation
+
+`$ brew tap carljavier/apps git@github.com:carljavier/homebrew-carljavier.git`
+
+
+
+## Available Formulas
+
+
+### [tfm](https://github.com/hashicorp-services/tfm)
+
+CLI tool for [tfm](https://github.com/hashicorp-services/tfm)
+
+`$ brew install carljavier/apps/tfm`
