5ae5c7daea206a78a40688413e8647887ca4cbc1
carljavier <cjavier@gmail.com>
2023-09-06 21:10:58 +1000

    Brew formula update for hava version 0.0.25-beta

--- Formula/hava.rb
+++ Formula/hava.rb
@@ -5,13 +5,13 @@
 class Hava < Formula
   desc "hava is a Go CLI tool to interact with app.hava.io platform."
   homepage "https://github.com/carljavier/hava"
-  version "0.0.24-beta"
+  version "0.0.25-beta"
   license "Apache-2.0"
   depends_on :macos
 
   on_macos do
-    url "https://github.com/carljavier/hava/releases/download/0.0.24-beta/hava_Darwin_all.tar.gz"
-    sha256 "051881d36b676436870cc39f3c1ff408f64b7adb9587ad5eb55366cca97631cc"
+    url "https://github.com/carljavier/hava/releases/download/0.0.25-beta/hava_Darwin_all.tar.gz"
+    sha256 "054f7b26a28f6e8853c0ed461fee8618e46fbc41d13da43b69d2bb0fe928b450"
 
     def install
       bin.install "hava"
